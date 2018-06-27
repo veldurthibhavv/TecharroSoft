48f12a3f7a8a75338379dfba3ee41666267b0f22
ramesh <ramesh.venkatpr@gmail.com>
2018-06-27 16:38:01 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-TOMCAT_HOST=192.168.1.2
-TOMCAT_USER=gamut-idc-node1
-TOMCAT_PWD=gamut
-TOMCAT_HOME=/home/gamut-idc-node1/Distros/apache-tomcat-8.5.11
+TOMCAT_HOST=192.168.43.130 
+TOMCAT_USER=ramesh
+TOMCAT_PWD=ramesh
+TOMCAT_HOME=/Softwares/apache-tomcat-8.5.24
 #
-WAR_FILE_PATH=target/gamutkart.war
+WAR_FILE_PATH=target/TecharroSoft.war
 
 echo "Bringing tomcat server down for deployment..."
 sleep 3
@@ -14,9 +14,9 @@ $TOMCAT_HOME/bin/shutdown.sh 1>/dev/null 2>&1
 
 #1>file.log 2>&1
 if [ -f $WAR_FILE_PATH ];then
-    echo "deploying gamutkart application..."
+    echo "deploying TecharroSoft application..."
     sleep 4
-    sshpass -p "Gamut" ssh $TOMCAT_USER@$TOMCAT_HOST
+    sshpass -p "ramesh" ssh $TOMCAT_USER@$TOMCAT_HOST
     scp  $WAR_FILE_PATH $TOMCAT_USER@$TOMCAT_HOST:$TOMCAT_HOME/webapps
 else
    echo "war file doesn't exist! Please check the build"
